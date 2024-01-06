0671e2792f2e995f5be96fe92b3ff34e4325550a
Josef Susík <62243819+JosephSusik@users.noreply.github.com>
2024-01-06 23:40:03 +0100

    Add files via upload

--- src/components/Poem.tsx
+++ src/components/Poem.tsx
@@ -7,10 +7,10 @@ function Poem() {
             <p>Mám vybrat... červenou či modrou, mám hrát zlou či dobrou?</p>
             <p>Těžko říci.</p>
             <p>Voda vřící je na lince, můj pes si spí jak v bavlnce</p>
-            <p>a jemu se zase zdá o té milence... Lence... či Věrce.</p>
+            <p>a jemu se zase zdá o té milence... Lence... či Věrce?</p>
             <p>Lehounce, pošeptám mu do ucha... TY HAJ....</p>
-            <p>Já zavírám se do klece z vlny a doufám že vlasy se mi nezvlní,</p>
-            <p>protože venku pr-ší a když pr-ší tak se leje.</p>
+            <p>Já zavírám se do klece z vlny a doufám, že vlasy se mi nezvlní,</p>
+            <p>protože venku pr-ší a když pr-ší, tak se leje.</p>
             <p>Pozor, jinak se mi ta káva rozleje.</p>
             <p>Jak to bylo, nejprve hladce a pak obrace,</p>
             <p>z pod polštáře slyším nepříjemné vibrace...</p>
